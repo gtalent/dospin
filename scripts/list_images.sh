6c1321e485b92bfeef3f9b3dc7fc1d85350f882f
Gary Talent <gtalent2@gmail.com>
2017-01-28 15:19:45 -0600

    Remove scripts

--- scripts/list_images.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env sh
-curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer `cat dospin.json | jq -r .ApiToken`" "https://api.digitalocean.com/v2/images?page=1&per_page=100&private=true" | jq .
--- scripts/list_volumes.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-echo Volumes
-curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer `cat dospin.json | jq -r .ApiToken`" "https://api.digitalocean.com/v2/volumes" | jq .
